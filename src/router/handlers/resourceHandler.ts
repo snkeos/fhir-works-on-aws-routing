76a086aba37d2cd6429ee9acadb90ffb3d6f1b0d
MarioDegen <51999287+MarioDegen@users.noreply.github.com>
2021-10-08 19:32:09 +0200

    Log search params

--- src/router/handlers/resourceHandler.ts
+++ src/router/handlers/resourceHandler.ts
@@ -80,9 +80,15 @@ export default class ResourceHandler implements CrudHandlerInterface {
         requestContext: RequestContext,
         tenantId?: string,
     ) {
+        let urlSearchParam = new URLSearchParams(queryParams);
+ 
+        urlSearchParam.forEach(function(value, key) {
+         
+            console.log(`Search params: key:${key}, value: ${value}`);
+        });
         const searchResponse = await this.searchService.searchResources(
             resourceType,
-            queryParams,
+            urlSearchParam,
             userIdentity,
             requestContext,
             tenantId,
