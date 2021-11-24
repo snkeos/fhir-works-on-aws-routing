1932647a23e134efa9ff6ca93380b231977eb72f
MarioDegen <51999287+MarioDegen@users.noreply.github.com>
2021-11-24 21:56:55 +0100

    reactivate validation added logging

--- src/router/validation/validationUtilities.ts
+++ src/router/validation/validationUtilities.ts
@@ -6,8 +6,10 @@
 import { Validator } from 'fhir-works-on-aws-interface';
 
 export async function validateResource(validators: Validator[], resource: any): Promise<void> {
+    console.log(`Validate resource using ${validators.length} validators.`)
     for (let i = 0; i < validators.length; i += 1) {
         // eslint-disable-next-line no-await-in-loop
         await validators[i].validate(resource);
     }
+    console.log(`validation done.`)
 }
