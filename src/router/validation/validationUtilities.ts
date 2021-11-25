7e5eb6993e87ef16badeb73720ee64f26de0809b
MarioDegen <51999287+MarioDegen@users.noreply.github.com>
2021-11-25 14:23:59 +0100

    removed Xray from validator

--- src/router/validation/validationUtilities.ts
+++ src/router/validation/validationUtilities.ts
@@ -2,15 +2,11 @@
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
  */
-const AWSXRay = require('aws-xray-sdk');
-
 import { Validator } from 'fhir-works-on-aws-interface';
 export async function validateResource(validators: Validator[], resource: any): Promise<void> {
-    const subsegment = AWSXRay.getSegment();
-    const newSubseg = subsegment.addNewSubsegment("validateResource");
     for (let i = 0; i < validators.length; i += 1) {
         // eslint-disable-next-line no-await-in-loop
         await validators[i].validate(resource);
     }
-    newSubseg.close()
+
 }
